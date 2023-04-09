4e82b756f7c36230ae7e729dd1a633c8f9c1c736
xingshiwei <shiwei.xing>
2023-04-09 14:39:06 +0800

    f

--- src/views/pageB/components/child.vue
+++ src/views/pageB/components/child.vue
@@ -9,20 +9,20 @@
 </template>
 
 <script setup>
-import { inject } from 'vue'
+    import { inject } from 'vue'
 
-const name = inject('name', 'hello') // 看看有没有值，没值的话就适用默认值（这里默认值是hello）
-const msg = inject('msg')
-const changeName = inject('changeName')
+    const name = inject('name', 'hello') // 看看有没有值，没值的话就适用默认值（这里默认值是hello）
+    const msg = inject('msg')
+    const changeName = inject('changeName')
 
-function handleClick() {
-  // 这样写不合适，因为vue里推荐使用单向数据流，当父级使用readonly后，这行代码是不会生效的。没使用之前才会生效。
-  // name.value = '雷猴'
+    function handleClick() {
+    // 这样写不合适，因为vue里推荐使用单向数据流，当父级使用readonly后，这行代码是不会生效的。没使用之前才会生效。
+    // name.value = '雷猴'
 
-  // 正确的方式
-  changeName('虎躯一震')
+    // 正确的方式
+    changeName('虎躯一震')
 
-  // 因为 msg 没被 readonly 过，所以可以直接修改值
-  msg.value = '世界'
-}
+    // 因为 msg 没被 readonly 过，所以可以直接修改值
+    msg.value = '世界'
+    }
 </script>
--- src/views/pageB/index.vue
+++ src/views/pageB/index.vue
@@ -5,18 +5,18 @@
 </template>
 
 <script setup>
-import { ref, provide, readonly } from 'vue'
-import Child from './components/child.vue'
+    import { ref, provide, readonly } from 'vue'
+    import Child from './components/child.vue'
 
-const name = ref('猛虎下山')
-const msg = ref('雷猴')
+    const name = ref('猛虎下山');
+    const msg = ref('雷猴');
 
-// 使用readonly可以让子组件无法直接修改，需要调用provide往下传的方法来修改
-provide('name', readonly(name))
+    // 使用readonly可以让子组件无法直接修改，需要调用provide往下传的方法来修改
+    provide('name', readonly(name))
 
-provide('msg', msg)
+    provide('msg', msg)
 
-provide('changeName', (value) => {
-  name.value = value
-})
+    provide('changeName', (value) => {
+    name.value = value
+    })
 </script>
